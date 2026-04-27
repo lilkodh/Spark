e46ee11ee94d2289de767808b13bbe37958d8a39
lilkodh <khaliddrihemdev@gmail.com>
2026-04-27 16:48:29 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Header from "./Header.jsx";
+import Body from "./body.jsx";
+export function gitApp() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
---
+++ src/body.jsx
@@ -0,0 +1,10 @@
+function Body (){
+    return(
+     <main>
+        <section>
+            <h1>WELCOME</h1>
+        </section>
+     </main>
+    );
+}
+export default Body
